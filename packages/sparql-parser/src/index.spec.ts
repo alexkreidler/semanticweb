2dc965098c393ce09066098e71be377a3560a53c
Alex Kreidler <alexkreidler2020@gmail.com>
2020-08-05 17:43:24 -0400

    work on select query

--- packages/sparql-parser/src/index.spec.ts
+++ packages/sparql-parser/src/index.spec.ts
@@ -7,13 +7,21 @@ test("can use millan parser", () => {
     WHERE { ?x ?y ?z }
     `)
 
-    console.log(res)
+    // console.log(res)
 
     expect(res).toBeDefined()
 })
 
 test("can parse basic query", () => {
     const q = `SELECT * WHERE {
+        ?s ?p ?o
+      }`
+
+    parse(q)
+})
+
+test("can parse query with from specifier", () => {
+    const q = `SELECT * FROM <http://example.org/dft.ttl> FROM NAMED <http://example.org/alice> WHERE {
         ?s ?p ?o
       }`
 
--- packages/sparql-parser/src/index.ts
+++ packages/sparql-parser/src/index.ts
@@ -4,12 +4,36 @@ export const SPARQL_VERSION = "1.1"
 import { W3SpecSparqlParser, traverse } from "millan" //"millan/dist/standalone/millan.sparql"
 import { IRecognitionException, CstNode } from "chevrotain"
 
-import { SparqlQuery } from "sparqljs"
+import { SparqlQuery, SelectQuery, Wildcard } from "sparqljs"
 
 export const sparqlParser = new W3SpecSparqlParser()
 
 export type Out = SparqlQuery
 export type ErrorList = IRecognitionException[]
+
+const selectQuery = (node: CstNode): SelectQuery => {
+    const sc = node.children["SelectClause"][0] as CstNode
+    if (!sc) {
+        throw new Error("No select clause in select query")
+    }
+    let vars
+    if ("Star" in sc.children) {
+        vars = [Wildcard]
+    } else if ("Var" in sc.children) {
+        const vs = sc.children["Var"]
+        console.log(vs)
+
+        vs.forEach((node: CstNode) => {
+            console.log(node)
+        })
+    }
+    return {
+        prefixes: {},
+        type: "query",
+        queryType: "SELECT",
+        variables: vars,
+    }
+}
 
 export const parse = (text: string): Out | ErrorList => {
     const out = sparqlParser.parse(text)
@@ -23,7 +47,11 @@ export const parse = (text: string): Out | ErrorList => {
             next(node.children["Query"])
         } else if (node.name == "Query") {
             if (node.children["SelectQuery"]) {
-                console.log(JSON.stringify(node, undefined, 2))
+                const first = node.children["SelectQuery"][0]
+                console.log(first)
+
+                selectQuery(first as CstNode)
+                // console.log(JSON.stringify(node, undefined, 2))
             }
         }
         // ctx.node
