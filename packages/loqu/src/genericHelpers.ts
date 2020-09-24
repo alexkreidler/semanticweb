0f9776a4f060fcc10bc4add905a0a45e52e30872
Alex Kreidler <alexkreidler2020@gmail.com>
2020-09-24 17:36:55 -0400

    converted into lib

---
+++ packages/loqu/src/genericHelpers.ts
@@ -0,0 +1,58 @@
+/** The name of this file is based no the GenericNode file in the Sweb browser. we may change it */
+import { RdfDereferencer } from "rdf-dereference"
+
+import rDeref from "rdf-dereference"
+
+import { prettyPrint, stream2String } from "./utils"
+
+import * as jsonld from "jsonld"
+
+import rdfSerializer from "rdf-serialize"
+import { JsonLd } from "jsonld/jsonld-spec"
+
+export async function dereferenceToFlatJsonLD(iri: string): Promise<JsonLd> {
+    const rDDeref = rDeref as RdfDereferencer
+    const { quads } = await rDDeref.dereference(iri)
+
+    const textStream = rdfSerializer.serialize(quads, { contentType: "application/ld+json" })
+
+    const out = await stream2String(textStream)
+
+    const obj = JSON.parse(out)
+    const flat = await jsonld.flatten(obj, {})
+    return flat
+}
+
+/** Returns a graph containing the IRIs and details (via a JSON-LD Frame) about a provided set of properties on a given class */
+export async function getProperties(classIRI: string, requestedProperties: [string], frame?: object): Promise<JsonLd> {
+    const flat = await dereferenceToFlatJsonLD(classIRI)
+    console.log(prettyPrint(flat))
+
+    if ("@id" in frame) {
+        console.error("The provided frame should not have an ID. This function uses requestedProperties as the ID list")
+    }
+
+    const framed = await jsonld.frame(
+        flat,
+        frame
+            ? {
+                  ...frame,
+                  ...{
+                      "@id": requestedProperties,
+                  },
+              }
+            : {
+                  "@context": {
+                      rdfs: "http://www.w3.org/2000/01/rdf-schema#",
+                  },
+                  "@id": requestedProperties,
+                  "rdfs:label": {},
+                  "rdfs:comment": {},
+              },
+        {
+            explicit: true,
+        }
+    )
+    console.log(prettyPrint(framed))
+    return framed
+}
